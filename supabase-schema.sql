b703c64f1cf4a66879d3eb22378ad75c6242707b
MARBRUDC <44482755+MARBRUDC@users.noreply.github.com>
2026-05-28 18:29:57 -0500

    Delete supabase-schema.sql

--- supabase-schema.sql
+++
@@ -1,3 +0,0 @@
-const http=require('http'),fs=require('fs'),path=require('path');
-const root=__dirname;const mime={'.html':'text/html','.js':'application/javascript','.css':'text/css'};
-http.createServer((req,res)=>{let p=req.url==='/'?'index.html':req.url.slice(1);let fp=path.join(root,p);if(!fs.existsSync(fp)) fp=path.join(root,'index.html');res.setHeader('Content-Type',mime[path.extname(fp)]||'text/plain');res.end(fs.readFileSync(fp));}).listen(5173,()=>console.log('Local: http://localhost:5173'));
