9fbd1f09e4d07c28f535f4307c80c9571a890cf2
MARBRUDC <44482755+MARBRUDC@users.noreply.github.com>
2026-05-28 18:29:09 -0500

    Delete build.js

--- build.js
+++
@@ -1,9 +0,0 @@
-const fs = require("fs");
-
-console.log("Generando build SERVITEC PRO V13...");
-
-if (!fs.existsSync("./index.html")) {
-    throw new Error("No existe index.html");
-}
-
-console.log("Build completado correctamente.");
